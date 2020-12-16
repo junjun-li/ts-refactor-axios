8ebd143702f6cc34dffd4b1a89ccecb33252656d
lijunjun <11776174@qq.com>
2020-12-17 00:28:11 +0800

    test

--- src/xhr.ts
+++ src/xhr.ts
@@ -8,5 +8,6 @@ export default function(config: AxiosRequestConfig) {
   // 一个可选的布尔参数，表示是否异步执行操作，默认为true。如果值为false，send()方法直到收到答复前不会返回。如果true，已完成事务的通知可供事件监听器使用。如果multipart属性为true则这个必须为true，否则将引发异常。
   request.open(method.toUpperCase(), url, true)
 
+
   request.send(data)
 }
